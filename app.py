9419a2b9e3cb8ac0b564c0588728f70c2097f968
Pacharapol Withayasakpunt <polv@polv.cc>
2024-07-31 21:34:48 +0700

    update

--- app.py
+++ app.py
@@ -10,24 +10,31 @@ db = load_db()
 if __name__ == "__main__":
 
     class Api:
-        def new_vocab(self):
-            r = dict(
-                db.execute(
-                    """
+        def new_vocab_list(self, count=20):
+            rs = []
+
+            for r in db.execute(
+                """
                     SELECT
                         simp,
                         REPLACE(GROUP_CONCAT(DISTINCT pinyin), ',', '; ') pinyin,
+                        GROUP_CONCAT(DISTINCT trad) trad,
                         json_group_array(english) en
                     FROM cedict
                     WHERE json_extract([data], '$.wordfreq') > 5
                     GROUP BY simp
-                    ORDER BY RANDOM() LIMIT 1
-                    """
-                ).fetchone()
-            )
-            r["en"] = json.loads(r["en"])
-            print(r)
-            return r
+                    ORDER BY RANDOM() LIMIT ?
+                    """,
+                (count,),
+            ):
+                r = dict(r)
+                r["en"] = json.loads(r["en"])
+                rs.append(r)
+
+            return rs
+
+        def new_vocab(self):
+            return self.new_vocab_list(1)[0]
 
     win = webview.create_window("Pinyin Quiz", "web/cjdict.html", js_api=Api())
     webview.start(lambda: win.evaluate_js("newVocab()"))
