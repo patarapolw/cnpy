5425ad85df90f03b49fee00c665fe9edbc172fed
Pacharapol Withayasakpunt <polv@polv.cc>
2025-04-03 23:12:14 +0700

    (feat) trad Chinese segmenter

--- cnpy/db.py
+++ cnpy/db.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 from regex import Regex, IGNORECASE
+import jieba
 
 from cnpy.dir import user_root, assets_root
 
@@ -22,3 +23,5 @@ assets_db.row_factory = sqlite3.Row
 
 radical_db = sqlite3.connect(assets_root / "radical.db", check_same_thread=False)
 radical_db.row_factory = sqlite3.Row
+
+jieba.set_dictionary(assets_root / "dict.txt.big")
