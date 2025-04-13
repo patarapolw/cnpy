102e46f8e0e75cd2e3daca5533529fd5543eb088
Pacharapol Withayasakpunt <polv@polv.cc>
2025-04-13 15:52:40 +0700

    feat: add filter options for Pinyin, known Hanzi, and names in the analyzer

--- web/analyze/analyze.js
+++ web/analyze/analyze.js
@@ -1,6 +1,7 @@
 //@ts-check
 
 import { api } from "../api.js";
+import { openInModal } from "../modal.js";
 import { openItem, searchPinyin, searchVoc, speak } from "../util.js";
 
 const elButtonSubmit = /** @type {HTMLButtonElement} */ (
@@ -15,19 +16,92 @@ const elAnalyzer = /** @type {HTMLElement} */ (
 );
 const elResult = /** @type {HTMLElement} */ (document.querySelector("#result"));
 
+const elFilter = /** @type {HTMLInputElement} */ (
+  document.querySelector("#filter")
+);
+const elIsShowPinyin = /** @type {HTMLInputElement} */ (
+  document.querySelector("#show-pinyin")
+);
+const elIsKnownHanzi = /** @type {HTMLInputElement} */ (
+  document.querySelector("#known-hanzi")
+);
+const elIsNamesOnly = /** @type {HTMLInputElement} */ (
+  document.querySelector("#names-only")
+);
+
+document.querySelectorAll('a[target="modal"]').forEach((a) => {
+  if (!(a instanceof HTMLAnchorElement)) return;
+  a.onclick = (ev) => {
+    ev.preventDefault();
+    openInModal(a.href, a.innerText.split(" ")[0]);
+  };
+});
+
+/** @type {{ v: string; pinyin: string }[]} */
+let allItems = [];
+
 elButtonSubmit.addEventListener("click", async (ev) => {
   ev.preventDefault();
 
   const { result } = await api.analyze(
     elAnalyzer.querySelector("textarea").value
   );
+  allItems = result;
+  elFilter.value = "";
 
+  document
+    .querySelectorAll("fieldset")
+    .forEach((el) => el.classList.remove("active"));
+
+  elResult.classList.add("active");
+
+  filterItems();
+});
+
+elButtonReset.addEventListener("click", (ev) => {
+  ev.preventDefault();
+
+  elAnalyzer.querySelector("textarea").value = "";
+
+  document
+    .querySelectorAll("fieldset")
+    .forEach((el) => el.classList.remove("active"));
+
+  elAnalyzer.classList.add("active");
+});
+
+elFilter.addEventListener("input", (ev) => {
+  ev.preventDefault();
+  if (!/^\p{sc=Han}*$/u.test(elFilter.value)) return;
+  filterItems();
+});
+
+/** @type {Set<string>} */
+let knownHanzi = null;
+elIsKnownHanzi.addEventListener("change", async (ev) => {
+  if (!knownHanzi) {
+    const stats = await api.get_stats();
+    knownHanzi = new Set(stats.all);
+  }
+  filterItems();
+});
+
+elIsShowPinyin.addEventListener("change", (ev) => {
+  const ol = elResult.querySelector("ol");
+  ol.classList.toggle("hide-pinyin", !elIsShowPinyin.checked);
+});
+
+elIsNamesOnly.addEventListener("change", (ev) => {
+  filterItems();
+});
+
+async function filterItems() {
   const ol = elResult.querySelector("ol");
   ol.textContent = "";
 
   /**
    *
-   * @param {(typeof result)[0]} r
+   * @param {(typeof allItems)[0]} r
    * @returns
    */
   const makeLI = (r) => {
@@ -74,23 +148,19 @@ elButtonSubmit.addEventListener("click", async (ev) => {
     return li;
   };
 
-  ol.append(...result.map((r) => makeLI(r)));
+  let items = elFilter.value.trim()
+    ? allItems.filter((r) => r.v.includes(elFilter.value))
+    : allItems;
 
-  document
-    .querySelectorAll("fieldset")
-    .forEach((el) => el.classList.remove("active"));
+  if (!elIsKnownHanzi.checked) {
+    items = items.filter((r) => {
+      return Array.from(r.v).some((c) => !knownHanzi.has(c));
+    });
+  }
 
-  elResult.classList.add("active");
-});
+  if (elIsNamesOnly.checked) {
+    items = items.filter((r) => /^[A-Z]/.test(r.pinyin));
+  }
 
-elButtonReset.addEventListener("click", (ev) => {
-  ev.preventDefault();
-
-  elAnalyzer.querySelector("textarea").value = "";
-
-  document
-    .querySelectorAll("fieldset")
-    .forEach((el) => el.classList.remove("active"));
-
-  elAnalyzer.classList.add("active");
-});
+  ol.append(...items.map((r) => makeLI(r)));
+}
